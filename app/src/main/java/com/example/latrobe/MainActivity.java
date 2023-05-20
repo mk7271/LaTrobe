8c60a73f681378abd5676dfd01f34cff19263566
Mayank <mayankrajput392@gmail.com>
2023-05-20 23:27:25 +0530

    Changed Main Activity Page to Login Page and connected Firebase

--- app/src/main/java/com/example/latrobe/MainActivity.java
+++ app/src/main/java/com/example/latrobe/MainActivity.java
@@ -1,14 +1,99 @@
 package com.example.latrobe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView cl;
+    EditText em,pd;
+    Button re;
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            startActivity(new Intent("com.example.Home"));
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cl= findViewById(R.id.regis_text);
+        cl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent("com.example.Register"));
+            }
+        });
+
+        mAuth= FirebaseAuth.getInstance();
+        em=findViewById(R.id.lg_em);
+        pd=findViewById(R.id.lg_pd);
+
+        re=findViewById(R.id.lg_btn);
+
+        re.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email,password;
+                email= String.valueOf(em.getText());
+                password= String.valueOf(pd.getText());
+
+
+                //Checking The values are filled or not
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(MainActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(MainActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+                //Adding User To the Firebase
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "LogIn Successful", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent("com.example.Home"));
+                                } else {
+
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+            }
+        });
     }
 }
